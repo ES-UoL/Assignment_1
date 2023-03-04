80813e67277b94ea122e4d989cbdf2d70fd88164
Earl Smalley <16617508@students.lincoln.ac.uk>
2023-03-04 19:34:41 +0000

    Implemented object-oriented design for the cards, pack, shuffle and deal functionality

--- CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/Pack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,26 +9,135 @@ namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        List<Card> pack;
+        //Fields
+        public static List<Card> pack = new List<Card>();
+        public static List<Card> packFirstHalf = new List<Card>();
+        public static List<Card> packSecondHalf = new List<Card>();
+        public static List<Card> dealtCards = new List<Card>();
+        public static List<Card> recentlyDealtCards = new List<Card>();
 
+        //Pack Constructor
         public Pack()
         {
-            //Initialise the card pack here
+            int x = 1;
+            int y = 1;
+            while(y != 5)
+            {
+                pack.Add(new Card(x, y));
+                x++;
+                if (x == 14)
+                {
+                    x = 1;
+                    y++;
+                }
+            }
         }
 
+        //Methods
+        //Shuffles the pack based on the type of shuffle
         public static bool shuffleCardPack(int typeOfShuffle)
         {
-            //Shuffles the pack based on the type of shuffle
+            //Fisher-Yates Shuffle
+            if (typeOfShuffle == 1)
+            {
+                Random random = new Random();
+                for (int i = 0; i < pack.Count; i++)
+                {
+                    int j = random.Next(i, pack.Count);
+                    Card temp = pack[i];
+                    pack[i] = pack[j];
+                    pack[j] = temp;
+                }
+                return true;
+            }
 
+            //Riffle Shuffle
+            else if (typeOfShuffle == 2)
+            {
+                packFirstHalf.AddRange(pack.GetRange(0, 26));
+                packSecondHalf.AddRange(pack.GetRange(26, 26));
+                int i = 0;
+                int j = 0;
+                while (i != 52)
+                {
+                    pack[i] = packFirstHalf[j];
+                    i++;
+                    pack[i] = packSecondHalf[j];
+                    i++;
+                    j++;
+                }
+                return true;
+            }
+
+            //No Shuffle
+            else if (typeOfShuffle == 3)
+            {
+                Console.WriteLine("The Pack was not shuffled and so the cards\nremain in the same order as they were before\n");
+                return true;
+            }
+
+            //Exception
+            else 
+            {
+                return false;
+            }
         }
-        public static Card deal()
+        //Deals one card
+        public static Card dealCard()
         {
-            //Deals one card
+            recentlyDealtCards.Clear();
+            if (pack.Count == 0)
+            {
+                Console.WriteLine("The deck is empty");
+                return null;
+            }
+            else
+            {
+                Card TopCard = pack[0];
+                pack.RemoveAt(0);
+                recentlyDealtCards.Add( TopCard );
+                dealtCards.AddRange(recentlyDealtCards);
+                return TopCard;
+            }
 
         }
+        //Deals the number of cards specified by 'amount'
         public static List<Card> dealCard(int amount)
         {
-            //Deals the number of cards specified by 'amount'
+            recentlyDealtCards.Clear();
+            if (pack.Count == 0)
+            {
+                Console.WriteLine("The deck is empty");
+                return null;
+            }
+            else if (pack.Count < amount)
+            {
+                Console.WriteLine($"It is not possible to deal {amount} cards since there are only {pack.Count} cards left in the deck");
+                return null;
+            }
+            else
+            {
+                for (int i = 1; i <= amount; i++)
+                {
+                    Card TopCard = pack[0];
+                    pack.RemoveAt(0);
+                    recentlyDealtCards.Add(TopCard);
+                }
+                dealtCards.AddRange(recentlyDealtCards);
+                return recentlyDealtCards;
+            }
+
+        }
+        // Display pack
+        public static void DisplayPack()
+        {
+            int count = 1;
+            foreach (Card card in pack)
+            {
+                Console.Write($"{count}. ");
+                card.WhatCardIsThis();
+                count++;
+            };
         }
     }
 }
---
+++ CMP1903M A01 2223/Testing.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Testing
+    {
+        static void Main(string[] args)
+        {
+            // Create a pack object
+            Pack CardPack = new Pack();
+
+            // Display the default order of the pack
+            Console.WriteLine("\n----------------Default ordered pack----------------\n");
+            Pack.DisplayPack();
+
+            // Shuffle types
+
+            // Riffle shuffle and display the current order of the pack
+            Console.WriteLine("\n----------------Riffle shuffle----------------\n");
+            bool ShuffleStatus = Pack.shuffleCardPack(2);
+            if (ShuffleStatus)
+            {
+                Console.WriteLine("Riffle shuffle successful");
+            }
+            Console.WriteLine("\nHere is the current order of the pack:\n");
+            Pack.DisplayPack();
+
+            // Fisher-Yates shuffle and display the current order of the pack
+            Console.WriteLine("\n----------------Fisher-Yates shuffle----------------\n");
+            bool ShuffleStatus2 = Pack.shuffleCardPack(1);
+            if (ShuffleStatus2)
+            {
+                Console.WriteLine("Fisher-Yates shuffle successful");
+            }
+            Console.WriteLine("\nHere is the current order of the pack:\n");
+            Pack.DisplayPack();
+
+            // No shuffle and display the current order of the pack
+            Console.WriteLine("\n----------------No shuffle----------------\n");
+            Pack.shuffleCardPack(3);
+            Console.WriteLine("Here is the current order of the pack:\n");
+            Pack.DisplayPack();
+
+            // Invalid shuffle type and display the current order of the pack
+            Console.WriteLine("\n----------------Invalid shuffle type----------------\n");
+            bool ShuffleStatus3 = Pack.shuffleCardPack(4);
+            if (!ShuffleStatus3)
+            {
+                int ShuffleType = 0;
+                while (ShuffleType != 1 && ShuffleType != 2 && ShuffleType != 3)
+                {
+                    Console.WriteLine("Error: Invalid shuffle type\n\nThe shuffle type number must be set to either:\n1 - Fisher-Yates shuffle\n2 - Riffle shuffle\n3 - No shuffle\n");
+                    Console.WriteLine("Please enter the valid shuffle type here:");
+                    ShuffleType = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                }
+                Pack.shuffleCardPack(ShuffleType);
+                Console.WriteLine("Here is the current order of the pack:\n");
+                Pack.DisplayPack();
+            }
+
+            // Deal methods
+
+            // Deal one card
+            Console.WriteLine("\n ----------------Deal and display one card---------------- \n");
+            Pack.dealCard().WhatCardIsThis();
+            Console.WriteLine("\n ----------------Display pack again after deal---------------- \n");
+            Pack.DisplayPack();
+
+            // Deal multiple cards
+            Console.WriteLine("\n ----------------Deal and display multiple cards---------------- \n");
+            Pack.dealCard(5);
+            foreach (Card card in Pack.recentlyDealtCards)
+            {
+                card.WhatCardIsThis();
+            }
+            Console.WriteLine("\n ----------------Display pack again after multi-deal---------------- \n");
+            Pack.DisplayPack();
+
+            // Display all dealt cards
+            Console.WriteLine("\n ----------------Display all dealt cards---------------- \n");
+            if (Pack.dealtCards.Count == 0)
+            {
+                Console.WriteLine("No cards have been dealt yet");
+            }
+            else
+            {
+                foreach (Card card in Pack.dealtCards)
+                {
+                    card.WhatCardIsThis();
+                }
+            }
+
+            // Pause program before termination
+            Console.ReadKey();
+        }
+    }
+}
